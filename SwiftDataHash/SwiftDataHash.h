4a9809ad8609210adab6305590453d0fb13f9947
William Kent <wjk011@gmail.com>
2015-01-06 18:31:29 -0500

    Initial Commit

---
+++ SwiftDataHash/SwiftDataHash.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDataHash.h
+//  SwiftDataHash
+//
+//  Created by William Kent on 1/6/15.
+//  Copyright (c) 2015 William Kent. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftDataHash.
+FOUNDATION_EXPORT double SwiftDataHashVersionNumber;
+
+//! Project version string for SwiftDataHash.
+FOUNDATION_EXPORT const unsigned char SwiftDataHashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataHash/PublicHeader.h>
+
+
